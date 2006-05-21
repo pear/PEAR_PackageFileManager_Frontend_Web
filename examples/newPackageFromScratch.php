bf2ea04e1862d937e2931e9ef14733a9c6ecc75e
Laurent Laville <farell@php.net>
2006-05-21 12:47:00 +0000

    new example
    git-svn-id: http://svn.php.net/repository/pear/packages/PEAR_PackageFileManager_Frontend_Web/trunk@213454 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ examples/newPackageFromScratch.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Make a new package from scratch.
+ *
+ * @category   PEAR
+ * @package    PEAR_PackageFileManager_Frontend_Web
+ * @author     Laurent Laville <pear@laurent-laville.org>
+ * @copyright  2005-2006 Laurent Laville
+ * @license    http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @version    CVS: $Id$
+ * @since      File available since Release 0.4.0
+ */
+
+require_once 'PEAR/PackageFileManager/Frontend.php';
+require_once 'Log.php';
+
+session_start();
+
+// logs only normal activity
+$output = $_ENV['TMP'] . DIRECTORY_SEPARATOR . basename(__FILE__,'.php');
+$logger = &Log::singleton('file', $output . '.log', '', array(), PEAR_LOG_INFO);
+
+// build empty instance of PFM web frontend
+$web =& PEAR_PackageFileManager_Frontend::singleton('Web', false, false, $logger);
+if ($web->hasErrors('error')) {
+    $errors = $web->getErrors();
+    echo '<pre>';
+    var_dump($errors);
+    echo '</pre>';
+    die('exit on Error');
+}
+$web->run();
+?>
