4d67b947a94536bdcbe8d30a293b018d694d1d48
Laurent Laville <farell@php.net>
2006-05-21 11:14:51 +0000

    new example to demonstrate how to use setOption() to define each option one by one rather than global options with loadPreferences()
    git-svn-id: http://svn.php.net/repository/pear/packages/PEAR_PackageFileManager_Frontend_Web/trunk@213441 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ examples/makePFMFrontend.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Make the PFM frontend package.
+ *
+ * @category   PEAR
+ * @package    PEAR_PackageFileManager_Frontend_Web
+ * @author     Laurent Laville <pear@laurent-laville.org>
+ * @copyright  2005-2006 Laurent Laville
+ * @license    http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @version    CVS: $Id$
+ * @since      File available since Release 0.1.0
+ */
+
+require_once 'PEAR/PackageFileManager/Frontend.php';
+require_once 'Log.php';
+
+session_start();
+
+// logs all activity
+$output = $_ENV['TMP'] . DIRECTORY_SEPARATOR . basename(__FILE__,'.php');
+$logger = &Log::singleton('file', $output . '.log');
+
+// where to find package sources
+$pkgDir = 'D:/php/pear/PEAR_PackageFileManager_Frontend/package2.xml';
+
+$web =& PEAR_PackageFileManager_Frontend::singleton('Web', $pkgDir, false, $logger);
+// configuration options
+$web->setOption('baseinstalldir', 'PEAR/PackageFileManager');
+$web->setOption('exportcompatiblev1', true);
+$web->setOption('changelogoldtonew', false);
+$web->setOption('simpleoutput', true);
+$web->setOption('outputdirectory', 'd:/temp');
+
+// add ability to dump some informations for debugging
+$web->addActions(array('dump' => 'ActionDump'));
+
+// stop if serious error(s)
+if ($web->hasErrors('error')) {
+    $errors = $web->getErrors();
+    echo '<pre>';
+    var_dump($errors);
+    echo '</pre>';
+    die('exit on Error');
+}
+// run the wizard tabbed pages
+$web->run();
+?>
