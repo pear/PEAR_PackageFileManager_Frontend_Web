832774634d9daef2e01e3b65a37c9a7f7f7426ef
Laurent Laville <farell@php.net>
2006-08-19 10:01:55 +0000

    new architechture (still compatible with old one) that allow to use an external stylesheet
    git-svn-id: http://svn.php.net/repository/pear/packages/PEAR_PackageFileManager_Frontend_Web/trunk@218463 c90b9560-bf6c-de11-be94-00142212c4b1

--- Frontend/Web/Default.php
+++ Frontend/Web/Default.php
@@ -25,15 +25,39 @@
 class ActionDisplay extends HTML_QuickForm_Action_Display
 {
     /**
+     * Style sheet for the custom layout
      *
-     * @since  0.1.0
+     * @var    string
+     * @access public
+     * @since  0.6.0
+     */
+    var $css;
+
+    /**
+     * class constructor
+     *
+     * @param  string  $css  custom stylesheet to apply, or default if not set
+     * @access public
+     * @since  0.6.0
+     */
+    function ActionDisplay($css = null)
+    {
+        // when no user-styles defined, used the default values
+        $this->setStyleSheet($css);
+    }
+
+    /**
+     * Outputs the form.
+     *
+     * @param  object HTML_QuickForm_Page  the page being processed
      * @access private
+     * @since  0.1.0
      */
     function _renderForm(&$page)
     {
-
         $formTemplate = "\n<form{attributes}>"
                       . "\n<table class=\"maintable\">"
+                      . "\n<caption>PEAR_PackageFileManager Web Frontend</caption>"
                       . "\n{content}"
                       . "\n</table>"
                       . "\n</form>";
@@ -51,8 +75,8 @@ class ActionDisplay extends HTML_QuickForm_Action_Display
                          . "\n</tr>";
 
         $elementTemplate = "\n<tr>"
-                         . "\n\t<td class=\"form-label\"><!-- BEGIN required --><span style=\"color: #ff0000\">*</span>&nbsp;<!-- END required --><!-- BEGIN label --><b>{label}</b><!-- END label --></td>"
-                         . "\n\t<td class=\"form-input\">{element}<!-- BEGIN error --><br /><span style=\"color: #ff0000\">{error}</span><!-- END error --></td>"
+                         . "\n\t<td class=\"form-label\"><!-- BEGIN required --><span class=\"required\">*</span>&nbsp;<!-- END required --><!-- BEGIN label -->{label}<!-- END label --></td>"
+                         . "\n\t<td class=\"form-input\">{element}<!-- BEGIN error --><br /><span class=\"error\">{error}</span><!-- END error --></td>"
                          . "\n</tr>";
 
         $groupTemplate = "\n\t\t<table cellspacing=\"0\">"
@@ -63,7 +87,7 @@ class ActionDisplay extends HTML_QuickForm_Action_Display
 
         $groupElementTemplate = "<td valign=\"top\">{element}"
                               . "<!-- BEGIN label --><br />"
-                              . "<span style=\"font-size:90%;\">{label}<!-- BEGIN required --><span style=\"color: #ff0000\">*</span><!-- END required --></span>"
+                              . "<span class=\"qfLabel\">{label}<!-- BEGIN required --><span class=\"required\">*</span><!-- END required --></span>"
                               . "<!-- END label -->"
                               . "</td>";
 
@@ -93,9 +117,10 @@ class ActionDisplay extends HTML_QuickForm_Action_Display
         $data_dir = '@data_dir@' . DIRECTORY_SEPARATOR
                   . '@package_name@' . DIRECTORY_SEPARATOR;
 
-        $styles = file_get_contents(PEAR_PACKAGEFILEMANAGER_FRONTEND_DATADIR . 'pfmfe.css');
+        $styles = $this->getStyleSheet();
         $js     = file_get_contents($data_dir . 'doSelection.js');
-        $body   = $renderer->toHtml();
+
+        $body = $renderer->toHtml();
 
         $html = <<<HTML
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
@@ -115,12 +140,53 @@ $js
 </script>
 </head>
 <body>
-<h1>PEAR_PackageFileManager Web Frontend</h1>
 $body
 </body>
 </html>
 HTML;
         echo $html;
+    }
+
+    /**
+     * Returns the custom style sheet to use for layout
+     *
+     * @param  bool  $content (optional) Either return css filename or string contents
+     * @return string
+     * @access public
+     * @since  0.6.0
+     */
+    function getStyleSheet($content = true)
+    {
+        if ($content) {
+            $styles = file_get_contents($this->css);
+        } else {
+            $styles = $this->css;
+        }
+        return $styles;
+    }
+
+    /**
+     * Set the custom style sheet to use your own styles
+     *
+     * @param  string  $css (optional) File to read user-defined styles from
+     * @return bool    true if custom styles, false if default styles applied
+     * @access public
+     * @since  0.6.0
+     */
+    function setStyleSheet($css = null)
+    {
+        // default stylesheet is into package data directory
+        if (!isset($css)) {
+            $this->css = '@data_dir@' . DIRECTORY_SEPARATOR
+                       . '@package_name@' . DIRECTORY_SEPARATOR
+                       . 'default.css';
+        }
+
+        $res = isset($css) && file_exists($css);
+        if ($res) {
+            $this->css = $css;
+        }
+        return $res;
     }
 }
 ?>
